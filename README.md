657aff1684939b0e9e19c54fe26933d2a4a4dc24
Breno Brito <brenorb@gmail.com>
2025-07-10 21:47:09 -0300

    First commit: Initial agent implementation

---
+++ README.md
@@ -0,0 +1,8 @@
+Provide a README file with a concise description of the approach used to tackle the problem. Include:
+a. details about any assumptions made during model development and training.
+b. model's performance, highlighting its strengths and weaknesses.
+c. potential improvements or extensions to the solution, such as refining the model
+architecture, etc.
+
+Example Interaction:
+Include at least three example interactions where the user asks a question (something similar to the provided dataset), and the model provides an answer.
---
+++ agent.py
@@ -0,0 +1,50 @@
+from typing import Optional
+
+import dspy
+from dotenv import load_dotenv
+
+load_dotenv()
+
+lm = dspy.LM(model="openai/gpt-4o-mini")
+dspy.configure(lm=lm)
+
+class QASignature(dspy.Signature):
+    """
+    Answer medical questions based on the context.
+    """
+    medical_question: str = dspy.InputField(description="The medical question to answer")
+    history: dspy.History = dspy.InputField(description="The history of the conversation")
+    context: Optional[str] = dspy.InputField(description="The context to use to answer the question")
+    # Output fields
+    answer: str = dspy.OutputField(description="The answer to the question")
+
+class Agent(dspy.Module):
+    def __init__(self):
+        self.qa = dspy.ChainOfThought(QASignature)
+
+    def forward(self, question: str, history: dspy.History) -> str:
+        # search for context
+        context = None # TODO: search for context
+        
+        result = self.qa(medical_question=question, history=history, context=context)
+        return result.answer
+
+
+
+if __name__ == "__main__":
+    COLOR_USER = "\033[94m"
+    COLOR_ASSISTANT = "\033[92m"
+    COLOR_RESET = "\033[0m"
+
+    agent = Agent()
+    history = dspy.History(messages=[])
+    
+    while True:
+        user_message = input(f"{COLOR_USER}User: {COLOR_RESET}")
+        if user_message.lower() == "q":
+            print("Bye!")
+            break
+
+        response = agent.forward(user_message, history)
+        history.messages.append({"question": user_message, "answer": response})
+        print(f"{COLOR_ASSISTANT}{response}{COLOR_RESET}")
